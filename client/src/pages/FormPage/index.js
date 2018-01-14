18d41e67036a5b2bc0b37062558a41f2c71df167
Aukai Tirrell <aukai.tirrell@gmail.com>
2018-01-13 16:28:34 -1000

    form page with modal working

--- client/src/pages/FormPage/index.js
+++ client/src/pages/FormPage/index.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
-import {Row, Col, Container,  Form, Button, Modal} from 'reactstrap';
+import {Row, Col, Form, FormGroup, Input, Label, Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 import {Link} from 'react-router-dom';
+import './index.css';
 
 const farmForms = require('../../data/farmForms.json').forms;
-
+const userData = require('../../data/userData.json');
 class FormPage extends Component {
     constructor(props){
         super(props);
         this.state = {
-            task: farmForms.filter(item => (item.uid == props.match.params.id))[0]
+            task: farmForms.filter(item => (item.uid == props.match.params.id))[0],
+            user: userData.user,
+            data: userData.data[props.match.params.id],
+            modal: false
         }
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle(){
+        this.setState({
+            modal: !this.state.modal
+        })
     }
 
     render(){
@@ -17,6 +28,32 @@ class FormPage extends Component {
         return (
             <div className="form-page-container">
                 <div>{this.state.task.title}</div>
+                <Button onClick={this.toggle} color="success" block>Create New Log</Button>
+                <div className="previous-logs-conntainer">
+                    {this.state.data.entries.map(entry => (
+                    <div>
+                        <p>{entry.date}</p>
+                        <p>{entry["water-source"]}</p>
+                    </div>
+                    ))}
+                </div>
+                <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                    <ModalHeader toggle={this.toggle}>Create New Log</ModalHeader>
+                    <ModalBody>
+                    <div className="input-form-container">
+                        <Form>
+                            {this.state.task.fields.map(item => (
+                            <FormGroup>
+                                <Label for={item.label} className="input-label">{item.label}</Label>
+                                <Input id={item.label} type={item.type}/>
+                            </FormGroup>
+                            ))}
+                        </Form>
+                    </div>
+                    </ModalBody>
+                    <ModalFooter>
+                    </ModalFooter>
+                </Modal>
             </div>
         )
     }
